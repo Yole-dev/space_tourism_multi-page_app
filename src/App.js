ca33eda247075daef16710bc4cb84002bf54b4c4
Yole <igbavagodswill4@gmail.com>
2025-04-17 18:57:37 +0100

    Finished building the components of the mobile view working on the other view-ports

--- src/App.js
+++ src/App.js
@@ -61,5 +61,22 @@ function Header() {
 }
 
 function Main() {
-  return <div className="main-body"></div>;
+  return (
+    <div className="main-body">
+      <div>
+        <div className="heading-text-content">
+          <p className="heading1">so, you want to travel to</p>
+          <p className="heading2">space</p>
+          <p className="hero-text">
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+
+        <div className="explore-btn">explore</div>
+      </div>
+    </div>
+  );
 }
